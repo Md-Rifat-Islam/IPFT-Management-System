4c3885cf572b81ab9804f6cd7a6e0d147a18a3ac
MD Rifat Islam <rifat010bushra@gmail.com>
2022-07-22 17:13:06 +0600

    prototypr added

--- Frontent/login.php
+++ Frontent/login.php
@@ -1,103 +1,88 @@
 <?php
-  session_start();
-  $wrongInfo = false;
-  $conn = oci_connect('IPFT', 'IPFT', 'localhost/xe')
+session_start();
+$wrongInfo = false;
+$conn = oci_connect('IPFT', 'IPFT', 'localhost/xe')
   or die(oci_error());
 
-  if(!$conn){
-    echo "not connected";
-  }else{
-    if($_SERVER['REQUEST_METHOD']=='POST'){
+if (!$conn) {
+  echo "not connected";
+} else {
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST["submit"])) {
       $username = $_POST['username'];
       $password = $_POST['password'];
-      $_SESSION['uname']= $username;
-      $_SESSION['profile'] = $username;      
-      $sql = "select * from dealer_info natural join info where dealer_id='$username' and password='$password'";
-      $stid=oci_parse($conn, $sql);
-      $r=oci_execute($stid);
-      $row= oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
-      if($row==NULL){
-        $sql = "select * from admin where username= '$username' and password= '$password'";
-        $stid=oci_parse($conn, $sql);
-        $r=oci_execute($stid);
-        $row= oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
-        if($row!=NULL){
-          header("Location: admin.php");
-        }else{
-          $wrongInfo=true;
+      
+      $_SESSION['uname']= $password;
+
+      $sql = "select * from soldier where lower(name)=lower('$username') and SOLDIER_NO='$password'";
+      $stid = oci_parse($conn, $sql);
+      $r = oci_execute($stid);
+      $row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
+      echo $username;
+      if ($row == NULL) {
+        $sql = "select * from officer where lower(name)=lower('$username') and BA_NO='$password' ";
+        $stid = oci_parse($conn, $sql);
+        $r = oci_execute($stid);
+        $row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
+        if ($row != NULL) {
+          header("Location: officer.php");
+        } else {
+          $wrongInfo = true;
         }
-      }else{
-        header("Location: dealer2.php");
+      } else {
+        header("Location: soldier.php");
       }
     }
   }
+}
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-    <title>IPFT - Management System</title>
-    <meta content="" name="description" />
-    <meta content="" name="keywords" />
+<head>
+  <meta charset="utf-8" />
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-    <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon" />
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+  <title>IPFT - Management System</title>
+  <meta content="" name="description" />
+  <meta content="" name="keywords" />
 
-    <!-- Google Fonts -->
-    <link
-      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-      rel="stylesheet"
-    />
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon" />
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
 
-    <!-- Vendor CSS Files -->
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
-    <link
-      href="assets/vendor/bootstrap/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link
-      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
-      rel="stylesheet"
-    />
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
-    <link
-      href="assets/vendor/glightbox/css/glightbox.min.css"
-      rel="stylesheet"
-    />
-    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
 
-    <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet" />
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
 
-    <!-- =======================================================
-  * Template Name: Gp - v4.8.0
-  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-  </head>
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet" />
+</head>
 
-  <body>
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top aaa">
-      <div
-        class="container d-flex align-items-center justify-content-lg-between"
-      >
-        <h1 class="logo me-auto me-lg-0">
-          <a href="index.html">IPFT<span>.</span></a>
-        </h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top aaa">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+      <h1 class="logo me-auto me-lg-0">
+        <a href="index.html">IPFT<span>.</span></a>
+      </h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
-        <nav id="navbar" class="navbar order-last order-lg-0">
-          <ul>
-            <!-- <li><a class="nav-link" href="index.html">Home</a></li> -->
-            <!-- <li><a class="nav-link scrollto" href="#about">About</a></li>
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <!-- <li><a class="nav-link" href="index.html">Home</a></li> -->
+          <!-- <li><a class="nav-link scrollto" href="#about">About</a></li>
             <li><a class="nav-link scrollto" href="#services">Services</a></li>
             <li>
               <a class="nav-link scrollto" href="#portfolio">Portfolio</a>
@@ -128,217 +113,79 @@
               </ul>
             </li>
             <li><a class="nav-link scrollto" href="#contact">Contact</a></li> -->
-          </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav>
-        <!-- .navbar -->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+      <!-- .navbar -->
 
-        <!-- <a href="login.html" class="get-started-btn scrollto">Login</a> -->
-      </div>
-    </header>
-    <!-- End Header -->
-    <main id="main">
-      <!-- ======= Contact Section ======= -->
-      <section id="contact" class="contact mt-5">
-        <div class="container" data-aos="fade-up">
-          <div class="section-title">
-            <h2>IPFT</h2>
-            <p>LOG IN</p>
-          </div>
+      <!-- <a href="login.html" class="get-started-btn scrollto">Login</a> -->
+    </div>
+  </header>
+  <!-- End Header -->
+  <main id="main">
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact mt-5">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>IPFT</h2>
+          <p>LOG IN</p>
+        </div>
 
-          <!-- <div>
-          <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
-        </div> -->
 
-          <div class="row mt-5">
-            <!-- <div class="col-lg-4">
-            <div class="info">
-              <div class="address">
-                <i class="bi bi-geo-alt"></i>
-                <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+        <div class="row mt-5">
+
+
+          <div class="col-lg-12 mt-5 mt-lg-0">
+            <form action="login.php" method="post" role="form" >   <!-- class="php-email-form" -->
+              <div class="text-center form-group mt-3">
+                <input type="text" class="form-control" name="username" id="username" placeholder="username" required >
+              </div>
+              <div class="text-center form-group mt-3">
+                <input type="text" class="form-control" name="password" id="password" placeholder="password" required >
               </div>
 
-              <div class="email">
-                <i class="bi bi-envelope"></i>
-                <h4>Email:</h4>
-                <p>info@example.com</p>
-              </div>
-
-              <div class="phone">
-                <i class="bi bi-phone"></i>
-                <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
-              </div>
-
-            </div>
-
-          </div> -->
-
-            <div class="col-lg-12 mt-5 mt-lg-0">
-              <form method="post" role="form" class="php-email-form">
-                <div class="text-center form-group mt-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="username"
-                    id="username"
-                    placeholder="username"
-                    required
-                  />
-                </div>
-                <div class="text-center form-group mt-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="password"
-                    id="password"
-                    placeholder="password"
-                    required
-                  />
-                </div>
-
-                <div class="text-center">
+              <!-- <div class="text-center">
                   <button type="submit">Login</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- End Contact Section -->
-    </main>
-    <!-- End #main -->
-
-    <!-- ======= Footer ======= -->
-    <footer id="footer">
-      <div class="footer-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6">
-              <div class="footer-info">
-                <h3>Gp<span>.</span></h3>
-                <p>
-                  A108 Adam Street <br />
-                  NY 535022, USA<br /><br />
-                  <strong>Phone:</strong> +1 5589 55488 55<br />
-                  <strong>Email:</strong> info@example.com<br />
-                </p>
-                <div class="social-links mt-3">
-                  <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                  <a href="#" class="facebook"
-                    ><i class="bx bxl-facebook"></i
-                  ></a>
-                  <a href="#" class="instagram"
-                    ><i class="bx bxl-instagram"></i
-                  ></a>
-                  <a href="#" class="google-plus"
-                    ><i class="bx bxl-skype"></i
-                  ></a>
-                  <a href="#" class="linkedin"
-                    ><i class="bx bxl-linkedin"></i
-                  ></a>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-2 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Home</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">About us</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Services</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Terms of service</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Privacy policy</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Our Services</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Web Design</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Web Development</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Product Management</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Marketing</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Graphic Design</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-4 col-md-6 footer-newsletter">
-              <h4>Our Newsletter</h4>
-              <p>
-                Tamen quem nulla quae legam multos aute sint culpa legam noster
-                magna
-              </p>
-              <form action="" method="post">
-                <input type="email" name="email" /><input
-                  type="submit"
-                  value="Subscribe"
-                />
-              </form>
-            </div>
+                </div> -->
+              <!-- <div class="text-center"><button type="submit" class="btn btn-primary btn-block">Login</button></div> -->
+              <input class="text-center mt-3"  type="submit" name="submit">
+            </form>
           </div>
         </div>
       </div>
+    </section>
+    <!-- End Contact Section -->
+  </main>
+  <!-- End #main -->
 
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
       <div class="container">
         <div class="copyright">
-          &copy; Copyright <strong><span>Gp</span></strong
+          &copy; Copyright <strong><span>IPFT</span></strong
           >. All Rights Reserved
         </div>
         <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          Designed for <a href="">Databaes Management system project</a>
         </div>
       </div>
     </footer>
-    <!-- End Footer -->
+  <!-- End Footer -->
 
-    <div id="preloader"></div>
-    <a
-      href="#"
-      class="back-to-top d-flex align-items-center justify-content-center"
-      ><i class="bi bi-arrow-up-short"></i
-    ></a>
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Vendor JS Files -->
-    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="assets/vendor/aos/aos.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
 
-    <!-- Template Main JS File -->
-    <script src="assets/js/main.js"></script>
-  </body>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+</body>
+
 </html>
--- Frontent/officer.php
+++ Frontent/officer.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+$ba_no = $_SESSION['uname'];
+$conn = oci_connect('IPFT', 'IPFT', 'localhost/xe')
+  or die(oci_error());
+
+if (!$conn) {
+  echo "not connected";
+} else {
+  $sql = "select * from officer where BA_NO='$ba_no' ";
+        $stid = oci_parse($conn, $sql);
+        oci_execute($stid);
+        $row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -38,13 +53,6 @@
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet" />
-
-    <!-- =======================================================
-  * Template Name: Gp - v4.8.0
-  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
   </head>
 
   <body>
@@ -98,7 +106,7 @@
         </nav>
         <!-- .navbar -->
 
-        <a href="login.html" class="get-started-btn scrollto">Log Out</a>
+        <a href="login.php" class="get-started-btn scrollto">Log Out</a>
       </div>
     </header>
     <!-- End Header -->
@@ -109,7 +117,7 @@
             <div class="container" data-aos="fade-up">
                 <div class="section-title">
                     <h2>OFFICER</h2>
-                    <p>OFFICER</p>
+                    <p>OFFICER <?php echo $row["NAME"] ?></p>
                 </div>
 
                 <div class="row mt-5">
@@ -159,111 +167,13 @@
 
     <!-- ======= Footer ======= -->
     <footer id="footer">
-      <!-- <div class="footer-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6">
-              <div class="footer-info">
-                <h3>IPFT<span>.</span></h3>
-                <p>
-                  Mirpur Cantonment <br />
-                  Mirpur, Dhaka<br /><br />
-                  <strong>Phone:</strong> +1 5589 55488 55<br />
-                  <strong>Email:</strong> mirpur@cantonment.com<br />
-                </p>
-                <div class="social-links mt-3">
-                  <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                  <a href="#" class="facebook"
-                    ><i class="bx bxl-facebook"></i
-                  ></a>
-                  <a href="#" class="instagram"
-                    ><i class="bx bxl-instagram"></i
-                  ></a>
-                  <a href="#" class="google-plus"
-                    ><i class="bx bxl-skype"></i
-                  ></a>
-                  <a href="#" class="linkedin"
-                    ><i class="bx bxl-linkedin"></i
-                  ></a>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-lg-2 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Home</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">About us</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Services</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Terms of service</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Privacy policy</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Our Services</h4>
-              <ul>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Web Design</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Web Development</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Product Management</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i> <a href="#">Marketing</a>
-                </li>
-                <li>
-                  <i class="bx bx-chevron-right"></i>
-                  <a href="#">Graphic Design</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-4 col-md-6 footer-newsletter">
-              <h4>Our Newsletter</h4>
-              <p>
-                Tamen quem nulla quae legam multos aute sint culpa legam noster
-                magna
-              </p>
-              <form action="" method="post">
-                <input type="email" name="email" /><input
-                  type="submit"
-                  value="Subscribe"
-                />
-              </form>
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <div class="container">
         <div class="copyright">
           &copy; Copyright <strong><span>IPFT</span></strong
           >. All Rights Reserved
         </div>
         <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-          Designed by <a href="">BootstrapMade</a>
+          Designed for <a href="">Databaes Management system project</a>
         </div>
       </div>
     </footer>
